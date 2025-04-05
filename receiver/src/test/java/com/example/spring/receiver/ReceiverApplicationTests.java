b6c62ef6ef5efbec33a4dd495e2d4465fcde4936
jakubkrhovjak <c_jkrhovjak@groupon.com>
2025-04-05 07:34:20 +0200

    first commit

---
+++ receiver/src/test/java/com/example/spring/receiver/ReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ transmitter/src/main/java/com/example/spring/transmiter/TransmiterApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.transmiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransmiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransmiterApplication.class, args);
+    }
+
+}
